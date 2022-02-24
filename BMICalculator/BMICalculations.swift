e9aa46e2f64585e1562e33aa287941c1464eddf0
David Cormell <d.cormell@etoncollege.org.uk>
2022-02-24 10:57:31 +0000

    Skeleton for task

--- BMICalculator/BMICalculations.swift
+++ BMICalculator/BMICalculations.swift
@@ -9,19 +9,10 @@ import Foundation
 
 class BMICalculations {
     func calculateBmi(weight: Double, height: Double) -> Double {
-        let bmi = (weight / height / height) * 10000
-        return bmi.rounded(toPlaces: 1)
+        return 0.0
     }
     
     func lookUpBmiClassification(bmi: Double) -> String {
-        if bmi < 18.5 {
-            return "Underweight"
-        } else if bmi <= 24.9 {
-            return "Healthy"
-        } else if bmi <= 29.9 {
-            return "Overweight"
-        } else {
-            return "Obese"
-        }
+        return ""
     }
 }
--- BMICalculator/ContentView.swift
+++ BMICalculator/ContentView.swift
@@ -8,48 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var height: Double?
-    @State private var weight: Double?
-    @State private var bmiMessage: String = ""
-    private let bmiCalculations = BMICalculations()
-    
     var body: some View {
-        VStack {
-            Image("PublicHealth")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            Form {
-                Section("Enter your height (cm) and weight (kg)") {
-                    TextField("Height (cm)", value: $height, format: .number)
-                        .keyboardType(.decimalPad)
-                    TextField("Weight (kg)", value: $weight, format: .number)
-                        .keyboardType(.decimalPad)
-                }
-                Section {
-                    Text("\(bmiMessage)")
-                }
-            }
-            Button("Calculate BMI", action: updateBmi)
-                .padding()
-            
-            
-        }
-    }
-        
-    func updateBmi() {
-        if let height = height {
-            if let weight = weight {
-                let bmi = bmiCalculations.calculateBmi(weight: weight, height: height)
-                let classification = bmiCalculations.lookUpBmiClassification(bmi: bmi)
-                bmiMessage = "Your BMI is: \(bmi)\nYou are \(classification)"
-            } else {
-                bmiMessage = "Please enter a valid weight"
-            }
-        } else {
-            bmiMessage = "Pleas enter a valid height"
-        }
-
+        Text("Get Cracking!")
     }
 }
 
