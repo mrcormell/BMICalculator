b3a86a2f3c3db0b0855da73f1c01925c559940e4
mrcormell <d.cormell@etoncollege.org.uk>
2022-02-24 09:30:06 +0000

    Initial MVP

--- BMICalculator/ContentView.swift
+++ BMICalculator/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var height: Int = 0
+    @State private var weight: Double = 0.0
+    @State private var bmiMessage: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("PublicHealth")
+                .resizable()
+            Form {
+                TextField("Height (cm)", value: $height, format: .number)
+                    .keyboardType(.numberPad)
+                TextField("Weight (kg)", value: $weight, format: .number)
+                    .keyboardType(.decimalPad)
+            }
+            Button("Calculate BMI", action: updateBmi)
+            Text("\(bmiMessage)")
+            
+        }
+    }
+        
+    func updateBmi() {
+        let bmi = weight / pow(Double(height), 2)
+        let classification = "Underweight"
+        bmiMessage = "Your BMI is: \(bmi)\n You are \(classification)"
     }
 }
 
